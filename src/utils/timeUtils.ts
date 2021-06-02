ffcdcc371ca9e8a6470dff7ca04be052cce2f54d
d.kakhrimanov@cn-consult-gmbh.de <d.kakhrimanov@cn-consult-gmbh.de>
2021-06-02 23:29:26 +0200

    date formatter util

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -32,3 +32,27 @@ export const addMinutes = (step: number, prevTime: string) => {
 
   return `${toTimeFormat(prevHours + left)}:${toTimeFormat(newMinutes)}`;
 };
+
+export const formatDate = (today: Date) => {
+  const options = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  } as const;
+
+  return new Intl.DateTimeFormat('de-DE', options).format(today);
+};
+
+export const extractDateValues = (dateStr: string) => {
+  const cleared = dateStr.replaceAll(/[.,]/g, '');
+  const list = cleared.split(' ');
+  return { day: list[0], date: Number(list[1]), month: list[2], year: Number(list[3]) };
+};
+
+export const getToday = () => new Date();
+
+export const getCurrentDate = () => {
+  const today = formatDate(getToday());
+  return extractDateValues(today);
+};
