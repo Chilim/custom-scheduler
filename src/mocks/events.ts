f99dfc48ed65b52f3cf6a7609eb8910af64717a0
d.kakhrimanov@cn-consult-gmbh.de <d.kakhrimanov@cn-consult-gmbh.de>
2021-06-02 19:06:40 +0200

    mocks

---
+++ src/mocks/events.ts
@@ -0,0 +1,39 @@
+const events = [
+  {
+    id: 1,
+    start: '2021-06-01 12:12:00',
+    end: '2021-06-01 12:42:00',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    title: 'First event',
+  },
+  {
+    id: 2,
+    start: '2021-06-01 12:12:00',
+    end: '2021-06-01 12:42:00',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    title: 'Second event',
+  },
+  {
+    id: 3,
+    start: '2021-06-01 12:12:00',
+    end: '2021-06-01 12:42:00',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    title: 'Third event',
+  },
+  {
+    id: 4,
+    start: '2021-06-01 12:12:00',
+    end: '2021-06-01 12:42:00',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    title: 'Fourth event',
+  },
+  {
+    id: 5,
+    start: '2021-06-01 12:12:00',
+    end: '2021-06-01 12:42:00',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    title: 'Fifth event',
+  },
+];
+
+export default events;
