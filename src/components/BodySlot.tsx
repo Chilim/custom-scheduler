c8d44358895b3a83b302f79a041df1060ab411f0
d.kakhrimanov@cn-consult-gmbh.de <d.kakhrimanov@cn-consult-gmbh.de>
2021-06-02 19:06:33 +0200

    render passed prop content

--- src/components/BodySlot.tsx
+++ src/components/BodySlot.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { Td } from '@chakra-ui/react';
+import { CalendarBodyCell, CalendarBodyCellType } from '../types';
 
 type PropsType = {
-  children: React.ReactNode;
+  params: CalendarBodyCell;
 };
 
-const BodySlot = ({ children }: PropsType) => {
-  return <Td border='1px solid orange'>{children}</Td>;
+const Cell = styled(Td)`
+  width: ${({ type }: { type: CalendarBodyCellType }) =>
+    type === 'timeCell' ? '80px' : 'inherit'};
+`;
+
+const BodySlot = ({ params }: PropsType) => {
+  const content = React.useMemo(() => {
+    if (params.type === 'timeCell') return params.payload.time;
+    return null;
+  }, [params]);
+
+  return (
+    <Cell type={params.type} border='1px solid orange'>
+      {content}
+    </Cell>
+  );
 };
 
 export default BodySlot;
