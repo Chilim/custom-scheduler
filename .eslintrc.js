309d923d86b146265c123638e34fd6f51c9614d4
davud <davud.kakhriman@gmail.com>
2021-06-03 23:37:52 +0200

    rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,21 @@
 module.exports = {
-  extends: [
-    "react-app",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
-  rules: {},
+	extends: [
+		'react-app',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	rules: {
+		'prettier/prettier': [
+			'warn',
+			{
+				singleQuote: true,
+				semi: true,
+				trailingComma: 'es5',
+				useTabs: true,
+			},
+		],
+		'prefer-const': 'warn',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+	},
 };
