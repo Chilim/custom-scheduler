0feb56b7008f1310a697e05db0d9df74eb72c465
d.kakhrimanov@cn-consult-gmbh.de <d.kakhrimanov@cn-consult-gmbh.de>
2021-06-03 21:39:28 +0200

    days controller component implemented

---
+++ src/components/toolbar/DaysControls.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Flex } from '@chakra-ui/react';
+
+type PropsType = {
+  next: () => void;
+  previous: () => void;
+};
+
+const DaysControl = ({ next, previous }: PropsType) => {
+  return (
+    <Flex>
+      <Button color='yellow.500' onClick={previous}>
+        Previous
+      </Button>
+      <Button color='telegram.500' onClick={next}>
+        Next
+      </Button>
+    </Flex>
+  );
+};
+
+export default DaysControl;
