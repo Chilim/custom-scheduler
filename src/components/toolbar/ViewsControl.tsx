571990410b87e60afdbb29f4d19415229f577c49
davud <davud.kakhriman@gmail.com>
2021-06-03 23:41:15 +0200

    selectView applied

--- src/components/toolbar/ViewsControl.tsx
+++ src/components/toolbar/ViewsControl.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Flex, Button } from '@chakra-ui/react';
+import { CalendarView } from '../../types';
 
-const ViewsControl = () => {
-  return (
-    <Flex ml='auto'>
-      <Button color='twitter.500'>Day</Button>
-      <Button color='whatsapp.500'>Week</Button>
-    </Flex>
-  );
+type PropsType = {
+	selectView: (newView: CalendarView) => void;
+};
+
+const ViewsControl = ({ selectView }: PropsType) => {
+	return (
+		<Flex ml='auto'>
+			<Button color='twitter.500' onClick={() => selectView('day')}>
+				Day
+			</Button>
+			<Button color='whatsapp.500' onClick={() => selectView('week')}>
+				Week
+			</Button>
+		</Flex>
+	);
 };
 
 export default ViewsControl;
