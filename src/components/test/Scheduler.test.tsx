079dd6c24e22a130078bc6aa9ecd8d981d242ecd
davud <davud.kakhriman@gmail.com>
2021-06-06 16:41:08 +0200

    props updated

--- src/components/test/Scheduler.test.tsx
+++ src/components/test/Scheduler.test.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { screen, render } from '@testing-library/react';
 import Grid from '../Grid';
 
 describe('Scheduler', () => {
   it('should schedular be in the document', async () => {
-    render(<Grid duration={30} date={new Date()} view='week' />);
+    render(<Grid duration={30} date={new Date()} view="week" />);
     const table = screen.getByRole('table');
     expect(table).toBeInTheDocument();
   });
