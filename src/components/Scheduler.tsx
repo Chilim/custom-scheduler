a99f2eeb9297426ace32f5f3447acb1913d63ee3
davud <davud.kakhriman@gmail.com>
2021-06-09 23:43:20 +0200

    crud functions over events

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -4,11 +4,16 @@ import Grid from './Grid';
 import Toolbar from './toolbar';
 import { DAYS_IN_WEEK, INITIAL_DATE } from '../constants';
 import { getPreviousDate, getNextDate } from '../utils/timeUtils';
-import { CalendarView } from '../types';
+import { CalendarView, GridEventType } from '../types';
 
-const Scheduler = () => {
+type PropsType = {
+  data: GridEventType[];
+};
+
+const Scheduler = ({ data }: PropsType) => {
   const [date, setDate] = React.useState<Date>(INITIAL_DATE);
   const [view, setView] = React.useState<CalendarView>('week');
+  const [events, setEvents] = React.useState<GridEventType[]>(data);
 
   const previous = () => {
     const newDate = getPreviousDate(date, view === 'week' ? DAYS_IN_WEEK : 1);
@@ -24,13 +29,30 @@ const Scheduler = () => {
     setView(newView);
   };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const updateEvent = (updatedEvt: GridEventType) => {
+    const updatedEvts = events.map((evt) => (evt.id === updatedEvt.id ? updatedEvt : evt));
+    setEvents(updatedEvts);
+  };
+
+  const deleteEvent = (id: number) => {
+    const filteredEvts = events.filter((evt) => evt.id === id);
+    setEvents(filteredEvts);
+  };
+
+  const createEvent = (newEventDate: Omit<GridEventType, 'id'>) => {
+    const newId = events.length + 1;
+    const updatedEvts = [...events, { ...newEventDate, id: newId }];
+    console.log('updatedEvts', updatedEvts);
+    setEvents(updatedEvts);
+  };
+
   const memoizedControls = React.useMemo(() => ({ next, previous, selectView }), [view]);
+  const memoizedEventActions = React.useMemo(() => ({ updateEvent, deleteEvent, createEvent }), []);
 
   return (
     <Flex flexDir="column" h="100vh">
       <Toolbar controls={memoizedControls} />
-      <Grid date={date} view={view} duration={30} />
+      <Grid date={date} view={view} duration={30} actions={memoizedEventActions} events={events} />
     </Flex>
   );
 };
