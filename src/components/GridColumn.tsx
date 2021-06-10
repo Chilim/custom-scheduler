fa206f60f26d0675fe7f512bafe22782398dfa47
davud <davud.kakhriman@gmail.com>
2021-06-11 01:08:25 +0200

    getColumnStyle applied

--- src/components/GridColumn.tsx
+++ src/components/GridColumn.tsx
@@ -6,6 +6,14 @@ type PropsType = {
   isTimeColumn: boolean;
 };
 
+const getColumnStyle = (isTimeColumn: boolean) => {
+  if (isTimeColumn) return { position: 'relative', width: '100px' };
+  return {
+    position: 'relative',
+    flex: 1,
+  };
+};
+
 const GridColumn = ({ children, isTimeColumn }: PropsType) => {
   return (
     <Box position="relative" w={isTimeColumn ? '100px' : '200px'} borderRight="1px solid black">
