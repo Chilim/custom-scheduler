467e9d1d04cba24260adb196a7bc03fe5e9dc134
davud <davud.kakhriman@gmail.com>
2021-06-19 01:36:11 +0200

    pass starting time

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Scheduler from './components';
 import events from './mocks/events';
 
 function App() {
-  return <Scheduler data={events} />;
+  return <Scheduler data={events} startFrom={'08:00'} />;
 }
 
 export default App;
